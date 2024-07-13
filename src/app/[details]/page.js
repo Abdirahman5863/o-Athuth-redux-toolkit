a8765da618cf2453339bbd51b976ad6174fa5398
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-07-13 16:34:16 +0300

    shadcn ui added

--- src/app/[details]/page.js
+++ src/app/[details]/page.js
@@ -1,27 +1,33 @@
 import { fetchdetails } from "@/actions";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 
 export default async function Details({params}) {
 console.log(params)
 const result = await fetchdetails(params.details)
 console.log(result)
   return (
-    <main>
-      <h1>{result.title}</h1>
+ <main>
+   <Card>
+    <CardContent>
+    <CardTitle>{result.title}</CardTitle>
       <div>
         <img src={result.thumbnail} alt={result.title}/>
       </div>
       <div className="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-6 m-0 p-10">
         {result.images.map((image) => (
+          <div key={image}>
           <img src={image} alt={result.title}/>
-        ))}
+      </div>  ))}
       </div>
       <div>
         <p>{result.price}</p>
       </div>
 
-      <div>
-        <p>{result.description}</p>
-      </div>
+      <CardDescription>
+        {result.description}
+      </CardDescription>
+    </CardContent>
+   </Card>
     </main>
   );
 }
